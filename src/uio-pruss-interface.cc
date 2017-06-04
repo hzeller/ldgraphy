fffecd75af76dadc302aa116b0be5678c16395cc
Henner Zeller <h.zeller@acm.org>
2017-06-04 15:28:05 -0700

    Non-tented via for laser driver for single-sided etching.

--- src/uio-pruss-interface.cc
+++ src/uio-pruss-interface.cc
@@ -1,21 +1,20 @@
 /* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
- * (c) 2016 Henner Zeller <h.zeller@acm.org>,
- *          Leonardo Romor <leonardo.romor@gmail.com>,
+ * (c) 2017 Henner Zeller <h.zeller@acm.org>,
  *
- * This file is part of BeagleG. http://github.com/hzeller/beagleg
+ * This file is part of LDGraphy. http://github.com/hzeller/beagleg
  *
- * BeagleG is free software: you can redistribute it and/or modify
+ * LDGraphy is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * BeagleG is distributed in the hope that it will be useful,
+ * LDGraphy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with BeagleG.  If not, see <http://www.gnu.org/licenses/>.
+ * along with LDGraphy.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Implementation of the hardware interface of the PRU using the uio_pruss
